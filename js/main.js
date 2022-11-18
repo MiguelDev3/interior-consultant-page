2bcd11dc5344b394d32393f5ae98f1d89473b5ea
MiguelDev3 <miguel.edu.chacon.3@gmail.com>
2022-11-18 01:13:07 -0500

    Modo Darkss

--- js/main.js
+++ js/main.js
@@ -2,13 +2,28 @@
     // Selectors DOM
     const $btnHamburger = d.querySelector(".btn-hamburger");
     const $menuNav = d.querySelector(".nav-list");
-    
+    const $btnChangeTheme = d.querySelector("#btn-change-theme");
+    const sunIcon = `<span class="material-symbols-outlined">
+    brightness_5
+    </span>`
+    const moonIcon = `<span class="material-symbols-outlined">
+    dark_mode
+    </span>`
+
     //Events
     $btnHamburger.addEventListener('click', () =>{
         let btnState = $btnHamburger.getAttribute("aria-expanded");
         $menuNav.classList.toggle('active');
         $btnHamburger.setAttribute("aria-expanded", btnState === 'true' ? false : true);
     });
+    $btnChangeTheme.addEventListener('click', () => {
+        d.querySelector('body').classList.toggle("dark");
+        if(d.querySelector('body').className === "dark"){
+            $btnChangeTheme.innerHTML = moonIcon;
+        }else{
+            $btnChangeTheme.innerHTML = sunIcon;
+        }
+    })
 
     d.addEventListener('click', (e) => {
         if(!e.target.matches('.nav-list a')) return false;
